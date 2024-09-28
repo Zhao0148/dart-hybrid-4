1f7dce261ea882dcfb2250089f62661f667b1349
Wallace <144630953+Zhao0148@users.noreply.github.com>
2024-09-28 16:47:01 -0400

    pass in fetch size

--- bin/hybrid_4.dart
+++ bin/hybrid_4.dart
@@ -2,13 +2,14 @@ import 'package:http/http.dart' as http;
 import 'dart:convert' as convert;
 
 main(List<String> arguments) async {
-  var data = await fetchUsers();
+
+  var data = await fetchUsers(size: 10);
   loopThroughUsers(data);
 }
 
-Future<List<dynamic>> fetchUsers() async {
+Future<List<dynamic>> fetchUsers({size}) async {
   try {
-    var url = Uri.parse('https://randomuser.me/api/?page=3&results=10&inc=name,id');
+    var url = Uri.parse('https://randomuser.me/api/?page=3&results=$size&inc=name,id');
     var response = await http.get(url);
     if (response.statusCode == 200) {
       var jsonResponse = await convert.jsonDecode(response.body)['results'] as List<dynamic>;
