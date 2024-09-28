4e3effcc53ba52abc7e19844bc67891445c54afb
Wallace <144630953+Zhao0148@users.noreply.github.com>
2024-09-28 16:40:29 -0400

    OK

---
+++ bin/hybrid_4.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+main(List<String> arguments) async {
+  var data = await fetchUsers();
+  loopThroughUsers(data);
+}
+
+Future<List<dynamic>> fetchUsers() async {
+  try {
+    var url = Uri.parse('https://randomuser.me/api/?page=3&results=10&inc=name,id');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      var jsonResponse = await convert.jsonDecode(response.body)['results'] as List<dynamic>;
+      return jsonResponse;
+    } else {
+      return [];
+    }
+  } catch (e) {
+    return [];
+  }
+}
+
+void loopThroughUsers(List<dynamic> people) {
+  for (var person in people) {
+    print("First Name: ${person['name']['first']} | Last Name: ${person['name']['last']} | ID: ${person['id']['value']}");
+  }
+}
